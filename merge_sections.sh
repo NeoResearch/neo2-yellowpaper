c386627505ffc0db2802bb8f128ab22c242f05aa
Igor Machado <igor.machado@gmail.com>
2019-02-26 01:53:54 -0300

    page breaks after sections on main document

--- merge_sections.sh
+++ merge_sections.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "" > green_paper.md
 cd sections
-for f in `ls`; do
+for f in `ls *.md`; do
+   echo "" >> ../green_paper.md
    cat $f >> ../green_paper.md
+   echo "" >> ../green_paper.md
+   echo "&nbsp; \newpage" >> ../green_paper.md
 done
 cd ..

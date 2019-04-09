a4b5d342bb1672d28e33d136b5891b3385b046c1
Igor Machado <igor.machado@gmail.com>
2019-04-09 14:09:17 -0300

    renamed preamble

--- merge_sections.sh
+++ merge_sections.sh
@@ -3,7 +3,7 @@ echo "" > yellow_paper.md
 cd sections
 for f in `ls *.md`; do
    echo "" >> ../yellow_paper.md
-   cat 00_Preamble.md >> ../yellow_paper.md
+   cat 00_Preamble >> ../yellow_paper.md
    yaml=`echo $f | cut -d'.' -f 1`
    #echo "carregando ${yaml}"
    python chapter_heading.py "${yaml}.yaml" >> ../yellow_paper.md
